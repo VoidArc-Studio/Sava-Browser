36da8c7db5685b686c7736c08ad2da7a3647c313
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-20 23:30:48 +0200

    Create settings.rs

---
+++ src/settings.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize)]
+pub struct Settings {
+    pub theme: String,
+    pub homepage: String,
+    pub ai_model: String,
+    pub block_ads: bool,
+    pub block_javascript: bool,
+    pub block_fingerprinting: bool,
+}
+
+impl Settings {
+    pub fn load() -> Self {
+        Settings {
+            theme: "dark".to_string(),
+            homepage: "https://startpage.com".to_string(),
+            ai_model: "default".to_string(),
+            block_ads: true,
+            block_javascript: false,
+            block_fingerprinting: true,
+        }
+    }
+
+    pub fn save(&self) {
+        // TODO: Zapisz do pliku JSON
+    }
+}
