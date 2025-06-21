daa3c0f84c5572be76b04f235f147fe35256b419
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-21 18:17:49 +0200

    Update bookmarks.rs

--- src/bookmarks.rs
+++ src/bookmarks.rs
@@ -1,38 +1,76 @@
 use serde::{Deserialize, Serialize};
 use crate::privacy::PrivacyManager;
+use std::fs;
+use std::io::{self, Write};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EncryptedField {
+    data: Vec<u8>,
+    nonce: [u8; 12],
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Bookmark {
-    url: String,
+    url: EncryptedField,
     title: String,
     folder: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct BookmarkManager {
     bookmarks: Vec<Bookmark>,
+    #[serde(skip)] // Nie serializujemy privacy managera!
     privacy: PrivacyManager,
 }
 
 impl BookmarkManager {
     pub fn new() -> Self {
+        let bookmarks = Self::load_from_file().unwrap_or_default();
         BookmarkManager {
-            bookmarks: vec![],
+            bookmarks,
             privacy: PrivacyManager::new(),
         }
     }
 
     pub fn add_bookmark(&mut self, url: String) {
-        let encrypted_url = self.privacy.encrypt_data(&url);
+        let (encrypted_data, nonce) = self.privacy.encrypt_data(&url);
         let bookmark = Bookmark {
-            url: String::from_utf8(encrypted_url).unwrap(),
+            url: EncryptedField { data: encrypted_data, nonce },
             title: "New Bookmark".to_string(), // TODO: Pobierz tytuł strony
             folder: "Default".to_string(),
         };
         self.bookmarks.push(bookmark);
-        // TODO: Zapisz do pliku
+        let _ = self.save_to_file();
     }
 
-    pub fn get_bookmarks(&self) -> &Vec<Bookmark> {
-        &self.bookmarks
+    pub fn get_bookmarks(&self) -> Vec<String> {
+        self.bookmarks.iter()
+            .map(|b| self.privacy.decrypt_data(&b.url.data, &b.url.nonce))
+            .collect()
     }
+
+    fn save_to_file(&self) -> io::Result<()> {
+        let serializable = BookmarkManagerSer {
+            bookmarks: self.bookmarks.clone(),
+        };
+        let json = serde_json::to_string_pretty(&serializable)?;
+        let mut file = fs::File::create("bookmarks.json")?;
+        file.write_all(json.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> Option<Vec<Bookmark>> {
+        if let Ok(data) = fs::read_to_string("bookmarks.json") {
+            if let Ok(deserialized) = serde_json::from_str::<BookmarkManagerSer>(&data) {
+                return Some(deserialized.bookmarks);
+            }
+        }
+        None
+    }
+}
+
+// Osobny typ do serializacji bez PrivacyManager
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+struct BookmarkManagerSer {
+    bookmarks: Vec<Bookmark>,
 }
