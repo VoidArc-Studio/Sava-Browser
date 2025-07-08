c2fbc8f677169924bd49f61570288718e75eb781
Michal92299 <michal.99967@gmail.com>
2025-07-08 19:32:11 +0200

    Create email_service.cr

---
+++ backend/src/email_service.cr
@@ -0,0 +1,6 @@
+module EmailService
+  def self.check_email(user : String, password : String) : String
+    # Implementacja IMAP/POP3 do pobierania poczty
+    "Email service not implemented yet"
+  end
+end
