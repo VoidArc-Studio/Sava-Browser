fc1a059b16e3534d333a7a2127489e0b38bdddca
Michal92299 <michal.99967@gmail.com>
2025-07-08 19:26:46 +0200

    Create main.js

---
+++ frontend/src/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('frontend/src/renderer/index.html');
+  win.webContents.openDevTools(); // Dla debugowania
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
